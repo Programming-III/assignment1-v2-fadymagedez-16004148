a77a78d57ee7e746f0b78943499421c44023316c
fadymagedez-16004148 <fady.magedez@gmail.com>
2025-11-11 10:03:18 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -5,6 +5,26 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
+#ifndef COURSE_H
+#define COURSE_H
+#include <iostream>
+class course{
+    String coursecode;
+            String coursename;
+            int maxstudents;
+            student* students;
+            int currentstudents
+            course();
+            course(String cc,String cn,int ms,student* st,int cs);
+            void addstudent(const student& s);
+            void displaycourseinfo();
+            void setcoursename();
+            void setmaxstudents();
+            void setcourrentstudents();
+            String getcoursename();
+            int getmaxstudents();
+            int getcurrentstudents();
+}
 
 
 
