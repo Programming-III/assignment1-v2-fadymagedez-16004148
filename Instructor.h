a99716a3fc6bf937c3804eb8899cf7d76efb313b
fadymagedez-16004148 <fady.magedez@gmail.com>
2025-11-11 09:57:20 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,6 +4,22 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
+#ifndef INSTRUCTOR_H
+#define INSTRUCTOR_H
+#include <iostream>
+class instructor{
+     String department;
+     int experienceYears;
+     instructor():person();
+     instructor(String d,int expy):person(n,i);
+     void person:: display();
+     void setdepartment();
+     void setexperinceyear();
+     string getdepartment();
+     int getexperinceyear();
+     ~instructor():person();
+      
+}
 
 
 
