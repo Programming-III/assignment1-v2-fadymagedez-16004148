3470d237c1ea3484322aa37ee04a3181af5f8351
fadymagedez-16004148 <fady.magedez@gmail.com>
2025-11-11 09:55:16 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -5,7 +5,27 @@
 #include <string>
 #using namespace std;
 //#write student class here
-
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+class student{
+    //attribuits
+    int yearlevel;
+    String major;
+    //constructors
+    student():person();
+    student(int yl,String m):person(n,i);
+    //methods
+    void setyearlevel();
+    void setmajor();
+    int getyearlevel();
+    String getmajor();
+    void person::display();
+    ~student():person()
+    
+    
+    
+}
 
 
 
