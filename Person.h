c2a101dd27ce76bdca59f9511ad6a04024de6c0f
fadymagedez-16004148 <fady.magedez@gmail.com>
2025-11-11 09:54:28 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,6 +5,23 @@
 using namespace std;
 
 //#write Person class here
+#ifndef PEROSON_H
+#define PEROSON_H
+#include <iostream>
+using namespace std;
+class person{
+     String name;
+    int id;
+    person();
+    person(String n, int i);
+    void display();
+    void setname();
+    void setid();
+    String getname();
+    int getid();
+    ~person();
+     
+}
 
 
 
